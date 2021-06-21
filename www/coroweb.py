3ab2902eeb04935fb43c4ec1662b644e75667683
dongjiajun <djjcool@qq.com>
2021-06-21 14:46:01 +0800

    更新完毕服务器响应

--- www/coroweb.py
+++ www/coroweb.py
@@ -1,11 +1,7 @@
-import functools
-import inspect
-import logging
-import os
-from apis import APIError
+import asyncio, os, inspect, logging, functools
 from urllib import parse
-
 from aiohttp import web
+from apis import APIError
 
 def get(path):
     """Define decorator @get('/path')
@@ -177,3 +173,35 @@ class RequestHandler(object):
             return r
         except APIError  as e:
             return dict(error=e.error,data=e.data,message=e.message)
+
+def add_static(app):
+    path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'static')
+    app.router.add_static('/static/',path)
+    logging.info("add static %s =>%s"%('/static/',path))
+#注册url处理函数
+def add_route(app,fn):
+    method=getattr(fn,'__method__',None)
+    path=getattr(fn,'__method__',None)
+    if path is None or method is None:
+        raise ValueError('@get or @post not defined in %s'%str(fn))
+    if not asyncio.iscoroutinefunction(fn) and not inspect.isgeneratorfunction(fn):
+        fn=asyncio.coroutine(fn)
+    logging.info('add route %s %s=>%s(%s)'%(method,path,fn.__name__,",".join(inspect.signature(fn).parameters.keys())))
+    app.router.add_route(method,path,RequestHandler(app,fn))
+#把多次add_route调用换成自动扫描
+def add_routes(app,module_name):
+    n=module_name.rfind(".")
+    if n==(-1):
+        mod = __import__(module_name,globals(),locals())
+    else:
+        name=module_name[n+1:]
+        mod=getattr(__import__(module_name[:n],globals(),locals(),name))
+    for attr in dir(mod):
+        if attr.startswith("_"):
+            continue
+        fn=getattr(mod,attr)
+        if callable(fn):
+            method=getattr(fn,"__method__",None)
+            path=getattr(fn,"__route__",None)
+            if method and path:
+                add_route(app,fn)
