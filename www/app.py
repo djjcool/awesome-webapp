94024a041ab6e3d39cddb5d51d71e1f21b7492fb
dongjiajun <djjcool@qq.com>
2021-06-21 23:59:56 +0800

    更新app.py

--- www/app.py
+++ www/app.py
@@ -1,6 +1,25 @@
-import logging;logging.basicConfig(level=logging.INFO)
-import asyncio,os,json,time
+import logging
+from ssl import Options;logging.basicConfig(level=logging.INFO)
+import asyncio,os,json,time,orm
+from datetime import datetime
 from aiohttp import web
+from jinja2 import Environment, FileSystemLoader
+from coroweb import add_routes, add_static
+
+
+def init_jinja2(app,**kwargs):
+    logging.info(" 初始化 jinja2...")
+    Options=dict(
+        autoescape=kwargs.get('autoescape', True),
+        block_start_string=kwargs.get('block_start_string', '{%'),
+        block_end_string=kwargs.get('block_end_string', '%}'),
+        variable_start_string=kwargs.get('variable_start_string', '{{'),
+        variable_end_string=kwargs.get('variable_end_string', '}}'),
+        auto_reload=kwargs.get('auto_reload', True)
+    )
+    path=kwargs.get('path',None)
+    if path is None:
+        path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'templates')
 
 async def index(request):
     return web.Response(body=b"<h1>Awesome</h1>'",content_type='text/html')
